0c8937efb4929b130ba992c84bfc588e68f251f1
zhangqili <xq123666@outlook.com>
2023-07-25 17:36:46 +0800

    test freertos with wifi

---
+++ mjpeg_cam_normal1/main.h
@@ -0,0 +1,31 @@
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "board.h"
+#include "bflb_gpio.h"
+#include "bflb_mtimer.h"
+#include "bflb_adc.h"
+#include "bflb_i2c.h"
+#include "bflb_irq.h"
+#include "bflb_cam.h"
+#include "bflb_uart.h"
+#include "bl616_glb.h"
+#include "bflb_dma.h"
+#include "bflb_mjpeg.h"
+#include "bflb_pwm_v2.h"
+#include "bflb_clock.h"
+#include "log.h"
+
+//#define USE_LVGL
+
+extern struct bflb_device_s *gpio;
+extern struct bflb_device_s *i2c0;
+extern struct bflb_device_s *cam0;
+extern struct bflb_device_s *mjpeg;
+extern struct bflb_device_s *pwm;
+
+extern uint8_t tcp_rec_buf[64];
+extern char *recv_data;
+
+#endif MAIN_H_
